14a4bcf508695417852d8cf34a384ff1f744e9e6
Cindy Woo <wooshun999@gmail.com>
2020-01-29 19:00:01 +0900

    200129_middleware

--- index.js
+++ index.js
@@ -13,8 +13,15 @@ const handleHome = (req, res)=> res.send("Hello from home");
 
 const handleProfile = (req,res) => res.send("You are on my profile");
 
+const betweenHome = (req, res, next) => {
+    console.log("I'am Between");
+    next();
+};
+
+app.use(betweenHome);
 
 app.get("/" , handleHome);
+
 app.get("/profile" , handleProfile);
 
 app.listen(4000, handleListening);
