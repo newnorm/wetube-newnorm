412d39938197fe09746aa5e41387b0d9decf5219
Cindy Woo <wooshun999@gmail.com>
2020-02-01 23:55:22 +0900

    200201_mvc pattern, controlloers and routers

--- app.js
+++ app.js
@@ -4,23 +4,26 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter} from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+
+// express framework execute
 const app = express();
 
-const handleHome = (req, res)=> res.send("Hello from home");
 
-const handleProfile = (req,res) => res.send("You are on my profile");
-
-app.use(cookieParser());
-app.use(bodyParser.json());
+// middlewares
+app.use(cookieParser()); //cookie 전달받는 미들웨어
+app.use(bodyParser.json()); // 클라이언트로부터 form 이나 json으로 전달된 정보 검사
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(helmet())
-app.use(morgan("dev"));
+app.use(helmet()) // 안전성 증가시키는 미들웨어
+app.use(morgan("dev")); // 어플리케이션에서 발생하는 일들의로그를 기록하는 미들웨어
 
-app.get("/" , handleHome);
+// router 사용 (3개로 분리해서 편의 증가)
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.get("/profile" , handleProfile);
-
-app.use("/user", userRouter);
 
 export default app;
