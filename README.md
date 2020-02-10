cb17aa1c8317252a1567f3ffc3bdc3ec76ba16bc
Cindy Woo <wooshun999@gmail.com>
2020-02-10 19:13:48 +0900

    db 의 video를 받아와 화면에 뿌리기, mixin

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # wetube 
 
 Cloning Youtube with Vanilla and NodeJs
+
+## pages :
+
+- [ ] Home
+- [x] Join
+- [x] Login 
+- [x] Search
+- [ ] User Detail
+- [x] Edit Profile
+- [x] Change Password
+- [x] Upload
+- [ ] Video Detail
+- [x] Edit Video
---
+++ db.js
@@ -0,0 +1,51 @@
+export const videos= [
+    {
+        id:324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        creater: {
+            id:121212,
+            name:"Newnorm",
+            email:"nn@ab.com"
+        }
+    },
+    {
+        id:123467,
+        title: 'Video super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        creater: {
+            id:121212,
+            name:"Newnorm",
+            email:"nn@ab.com"
+        }
+    },
+    {
+        id:865434,
+        title: 'Video nice',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        creater: {
+            id:121212,
+            name:"Newnorm",
+            email:"nn@ab.com"
+        }
+    },
+    {
+        id:4567896,
+        title: 'Video perfect',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        creater: {
+            id:121212,
+            name:"Newnorm",
+            email:"nn@ab.com"
+        }
+    }         
+
+];
